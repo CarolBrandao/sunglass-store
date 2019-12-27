337d960bd6cca1102d579ec908fca653c39495c2
carolbrandao <bcarol744@gmail.com>
2019-12-27 18:31:20 +0100

    Create overlay effect on hover product thumbnail

--- src/product-card.js
+++ src/product-card.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
 
-const Product = styled.div`
-	border: solid 1px white;
+const Product = styled.a`
+	cursor: pointer;
 `
 
 const Sunglass = styled.div`
@@ -11,8 +11,8 @@ const Sunglass = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-	background-image:${props => `url(${props.src})`} ;
-
+	background-image: ${props => `url(${props.src})`};
+	position: relative;
 	${props => 
   	`${props.theme.mq[1]}{
       height: 650px;
@@ -24,6 +24,36 @@ const Sunglass = styled.div`
      }`
   }
 
+`
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: rgba(255, 255, 255, 0.8);
+
+  &:hover {
+    opacity: 1;
+  }
+`
+
+const TextOverlay = styled.div`
+	color: rgba(0, 0, 0, 0.5);;
+  font-size: ${props => props.theme.fontSizes[1]}px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+  text-transform: uppercase;
 `
 
 const Title = styled.span`
@@ -47,12 +77,14 @@ const Description = styled.div`
 export function ProductCard(props){
 	return (
 		<Product>
-			<Sunglass src={`thumbnails/${props.title}.jpg`} />
-				<Title>{props.title}</Title>
-				<Description>
-					<span>{props.description}</span>
-					<span>{props.price}</span>
-				</Description>
+			<Sunglass src={`thumbnails/${props.title}.jpg`}>
+				<Overlay><TextOverlay>Available in </TextOverlay></Overlay>
+			</Sunglass>
+			<Title>{props.title}</Title>
+			<Description>
+				<span>{props.description}</span>
+				<span>{props.price}</span>
+			</Description>
 		</Product>
 	)
 }
